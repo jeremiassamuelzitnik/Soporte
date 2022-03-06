b9d391aa0f9eb54091c95ea027d45678d3599354
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2022-03-06 17:38:13 -0300

    Create ScriptGeneral.ps1

---
+++ ScriptGeneral.ps1
@@ -0,0 +1,4 @@
+md $env:TEMP\JeremosTKBio\
+wget https://raw.githubusercontent.com/jeremiassamuelzitnik/Soporte/master/General.zip -OutFile $env:TEMP\JeremosTKBio\General.zip
+Expand-Archive -Path $env:TEMP\JeremosTKBio\General.Zip -DestinationPath $env:TEMP\JeremosTKBio\ -force
+Start-Process  $env:TEMP\JeremosTKBio\General\script` general.cmd
