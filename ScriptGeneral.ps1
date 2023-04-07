a50ef49f0dc484916d5824431aabdf17a4edf68a
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2023-04-07 03:48:53 -0300

    Update ScriptGeneral.ps1

--- ScriptGeneral.ps1
+++ ScriptGeneral.ps1
@@ -2,4 +2,5 @@ md $env:TEMP\JeremosGIT\
 cls
 wget https://raw.githubusercontent.com/jeremiassamuelzitnik/Soporte/master/General.zip -OutFile $env:TEMP\JeremosGIT\General.zip
 Expand-Archive -Path $env:TEMP\JeremosGIT\General.Zip -DestinationPath $env:TEMP\JeremosGIT\ -force
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -force
 .$env:TEMP\JeremosGIT\General\Script-general.ps1
