a21e6f7818ac574d28ef06f6a21583f9987fcec0
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2023-04-15 21:50:48 -0300

    Update ScriptGeneral.ps1

--- ScriptGeneral.ps1
+++ ScriptGeneral.ps1
@@ -1,6 +1,7 @@
 mkdir $env:TEMP\JeremosGIT\
-cls
-Invoke-WebRequest https://raw.githubusercontent.com/jeremiassamuelzitnik/Soporte/master/General.zip -OutFile $env:TEMP\JeremosGIT\General.zip
+Clear-Host
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://github.com/jeremiassamuelzitnik/Soporte/raw/main/General.zip", "$env:temp\JeremosGIT\General.zip")
 Expand-Archive -Path $env:TEMP\JeremosGIT\General.Zip -DestinationPath $env:TEMP\JeremosGIT\ -force
 Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -force
 .$env:TEMP\JeremosGIT\General\Script-general.ps1
