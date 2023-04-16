13a15ad3beb962a1c6831d7344cd818c0ce7c971
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2023-04-15 21:39:43 -0300

    .

--- ScriptGeneral.ps1
+++ ScriptGeneral.ps1
@@ -1,6 +1,6 @@
-md $env:TEMP\JeremosGIT\
+mkdir $env:TEMP\JeremosGIT\
 cls
-wget https://raw.githubusercontent.com/jeremiassamuelzitnik/Soporte/master/General.zip -OutFile $env:TEMP\JeremosGIT\General.zip
+Invoke-WebRequest https://raw.githubusercontent.com/jeremiassamuelzitnik/Soporte/master/General.zip -OutFile $env:TEMP\JeremosGIT\General.zip
 Expand-Archive -Path $env:TEMP\JeremosGIT\General.Zip -DestinationPath $env:TEMP\JeremosGIT\ -force
 Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -force
 .$env:TEMP\JeremosGIT\General\Script-general.ps1
