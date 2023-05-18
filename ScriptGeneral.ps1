033662e418fdfde1b9b0c3d53856ec1148295e06
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2023-05-18 13:05:37 -0300

    Update ScriptGeneral.ps1

--- ScriptGeneral.ps1
+++ ScriptGeneral.ps1
@@ -1,5 +1,4 @@
-mkdir $env:TEMP\JeremosGIT\
-Clear-Host
+mkdir "$env:temp\JeremosGIT\" -ErrorAction SilentlyContinue
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://github.com/jeremiassamuelzitnik/Soporte/raw/main/General.zip", "$env:temp\JeremosGIT\General.zip")
 Expand-Archive -Path $env:TEMP\JeremosGIT\General.Zip -DestinationPath $env:TEMP\JeremosGIT\ -force
